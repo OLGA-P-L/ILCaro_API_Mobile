8402e9d287c2bb433ef8ac760ca2e1c63a9714af
OLGA-P-L <loyolya@gmail.com>
2024-10-30 21:23:29 +0200

    Registration tests and AddNewCar tests

---
+++ src/main/java/interfaces/Base_Api.java
@@ -0,0 +1,20 @@
+package interfaces;
+
+import com.google.gson.Gson;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+
+public interface Base_Api {
+
+    String BASE_URL = "https://ilcarro-backend.herokuapp.com";
+    String REG_URL = "/v1/user/registration/usernamepassword";
+    String LOG_URL = "/v1/user/login/usernamepassword";
+    String ADD_NEW_CAR = "/v1/cars";
+
+
+
+    Gson GSON = new Gson();
+    MediaType JSON = MediaType.get("application/json");
+    OkHttpClient OK_HTTP_CLIENT = new OkHttpClient();
+
+}
